3b1342fa2c2483c3643e367fa3dfd92cf895b73f
manojchilagani <31198616+manojchilagani@users.noreply.github.com>
2018-07-10 14:27:20 +0530

    Add files via upload

---
+++ week3/day-1/5sideddie.py
@@ -0,0 +1,21 @@
+import random
+
+
+def rand7():
+    return random.randint(1, 7)
+
+
+def rand5():
+
+    # Implement rand5() using rand7()
+    while True:
+        r = rand7()
+        print r
+        if r < 6:
+            return r
+
+    # return 0
+
+
+print 'Rolling 5-sided die...'
+print rand5()
